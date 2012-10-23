a40d127693f45594418dfcc4e88eb6bc9e726fc5
Vladimir Ciobanu <github@cvlad.info>
2012-10-24 00:22:22 +0300

    Initial test.

---
+++ src/Test/Test.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace Test.Web.App_Start
+{
+    using System.Web.Optimization;
+
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            "~/Scripts/jquery-1.8.2.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+        }
+    }
+}
---
+++ src/Test/Test.Web/SignalR/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using Microsoft.Web.WebPages.OAuth;
+using SignalR.Hubs;
+
+namespace Test.Web.SignalR
+{
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        public void Join()
+        {
+            var name = GetUsername();
+            Clients.onJoined(name);
+        }
+
+        public void Message(string message)
+        {
+            var user = GetUsername();
+            Clients.onMessage(user, message);
+        }
+
+        private string GetUsername()
+        {
+            if(OAuthWebSecurity.IsAuthenticatedWithOAuth)
+                
+            return HttpContext.Current.User.Identity.Name;
+        }
+    }
+}
